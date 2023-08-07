4360c8ae03fecf913e05cc396a1af3ff88e4bf91
Zya rexx <raka_088@smkassalaambandung.sch.id>
2023-08-07 01:55:27 +0000

    Fix react/no-unescaped-entities

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,8 +8,8 @@ export default function ContactPage() {
     <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
 	<div className="flex flex-col justify-between">
 		<div className="space-y-2">
-			<h2 className="text-4xl font-bold leadi lg:text-5xl">Let's talk!</h2>
-			<div className="dark:text-gray-400">Let's start a conversation with me.</div>
+			<h2 className="text-4xl font-bold leadi lg:text-5xl">{"Let's talk!"}</h2>
+			<div className="dark:text-gray-400">{"Let's start a conversation with me."}</div>
 		</div>
 	</div>
 	<form className="space-y-6">
